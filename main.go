23cd72f54c0ea61f647df7fc3238da4114b724b9
Darren Lau <panyuenlau@gmail.com>
2020-10-19 11:24:26 -0400

    update proto import path and server response message

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"net"
 
-	pb "./proto"
+	pb "github.com/panyuenlau/mygrpc-server/proto"
 	grpc "google.golang.org/grpc"
+	peer "google.golang.org/grpc/peer"
 )
 
 const (
@@ -18,11 +19,20 @@ type server struct {
 }
 
 func (s *server) SayHello(ctx context.Context, request *pb.Request) (*pb.Response, error) {
-	log.Printf("Request Received: %v", request.GetReqeustMessage())
-	return &pb.Response{ReplyMessage: "Hello Client! I received your request of \"" + request.GetReqeustMessage() + "\""}, nil
+	p, _ := peer.FromContext(ctx) // retreieve the peer information in ctx if it exists
+	clientNetwork := p.Addr.Network()
+	clientIP := p.Addr.String()
+	requestMsg := request.GetReqeustMessage()
+
+	log.Printf("Request Received: \"%v\" from client: %s through %s", request.GetReqeustMessage(), clientIP, clientNetwork)
+
+	return &pb.Response{
+		ReplyMessage: "Hello Client! " + "I received your message of \"" + requestMsg + "\"",
+	}, nil
 }
 
 func main() {
+	log.Println("Waiting for requests from clients...")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("ERROR: failed to listen: %v", err)
